b73d793c2d1f07aa635f4cfad37a900002afb95f
vrushyy2010 <rautvrushali3289@gmail.com>
2024-04-07 22:58:29 +0530

    added class

--- Group_C_Calculator/src/com/calculator/MainCall.java
+++ Group_C_Calculator/src/com/calculator/MainCall.java
@@ -1,11 +1,31 @@
 package com.calculator;
 
+import java.util.Scanner;
+
 public class MainCall {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("khushalk");
-		System.out.println("hello       ");
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your Operation");
+        String operation = sc.nextLine();
+        
+		switch (operation) {
+		case "div":
+			DivisionOp division = new DivisionOp();
+			division.division();
+			break;
+		case "mod":
+			ModuloOp mod=new ModuloOp();
+			mod.modulo();
+			break;
+		case "square":
+			SquareOp sq=new SquareOp();
+			sq.square();
+			break;
+			
 
+		default:
+			System.out.println("Invalid operation");
+		}
+	}
 }
